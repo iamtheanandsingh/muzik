17f9fad0fedce27e5272c6679f4683abac03fccc
Anand <iamtheanandsingh@gmail.com>
2021-11-14 23:07:50 +0530

    added app with splash screen and icon

---
+++ lib/main.dart
@@ -0,0 +1,20 @@
+import 'package:flutter/material.dart';
+import 'package:animated_splash_screen/animated_splash_screen.dart';
+import 'package:muzik/screen/app.dart';
+
+void main() {
+  runApp(MaterialApp(
+    title: 'Muzik',
+    debugShowCheckedModeBanner: false,
+    home: AnimatedSplashScreen(
+      duration: 1000,
+      backgroundColor: Colors.grey,
+      splash: const CircleAvatar(
+        backgroundColor: Colors.transparent,
+        foregroundImage: AssetImage('assets/splash.png'),
+      ),
+      nextScreen: const MyApp(),
+      splashTransition: SplashTransition.scaleTransition,
+    ),
+  ));
+}
---
+++ lib/screen/app.dart
@@ -0,0 +1,52 @@
+import 'package:flutter/material.dart';
+import 'package:curved_navigation_bar/curved_navigation_bar.dart';
+import 'package:muzik/screen/home.dart';
+import 'package:muzik/screen/search.dart';
+import 'package:muzik/screen/yourlibrary.dart';
+
+class MyApp extends StatefulWidget {
+  const MyApp({Key? key}) : super(key: key);
+
+  @override
+  _MyAppState createState() => _MyAppState();
+}
+
+class _MyAppState extends State<MyApp> {
+
+  int index = 0;
+  final GlobalKey<CurvedNavigationBarState> _bottomNavigationKey = GlobalKey();
+
+  final screens = [
+    Home(),
+    Search(),
+    Library(),
+  ];
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: Colors.black,
+      body: screens[index],
+      bottomNavigationBar: CurvedNavigationBar(
+        key: _bottomNavigationKey,
+        index: index,
+        height: 60.0,
+        items: const <Widget>[
+          Icon(Icons.home_outlined, size: 30, color: Colors.black),
+          Icon(Icons.search, size: 30, color: Colors.black),
+          Icon(Icons.library_add_outlined, size: 30, color: Colors.black),
+        ],
+        color: Colors.grey,
+        backgroundColor: Colors.transparent,
+        animationCurve: Curves.easeInOutCubic,
+        animationDuration: const Duration(milliseconds: 200),
+        onTap: (index) {
+          setState(() {
+            this.index = index;
+          });
+        },
+        letIndexChange: (index) => true,
+      ),
+    );
+  }
+}
