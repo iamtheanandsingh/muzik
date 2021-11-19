b3a3ded01ed7452376d9609b303aae6caf7879fa
Anand Singh <iamtheanandsingh@gmail.com>
2021-11-19 18:36:34 +0530

    Added webpage isea along with basiic ui for home screen

--- muzik.app/lib/screen/home.dart
+++ muzik.app/lib/screen/home.dart
@@ -8,8 +8,40 @@ class Home extends StatefulWidget {
 }
 
 class _HomeState extends State<Home> {
+  createAppBar(String message) {
+    return AppBar(
+      backgroundColor: Colors.transparent,
+      elevation: 0.0,
+      title: Text(message),
+      actions: const [
+        Padding(
+            padding: EdgeInsets.only(right: 20), child: Icon(Icons.settings)),
+      ],
+    );
+  }
+
   @override
   Widget build(BuildContext context) {
-    return Container();
+    return SafeArea(
+      child: Container(
+        child: Column(
+          children: [
+            createAppBar("Good Morning"),
+          ],
+        ),
+        decoration: BoxDecoration(
+          gradient: LinearGradient(
+            colors: [
+              Colors.blueGrey.shade100,
+              Colors.black,
+              Colors.black,
+              Colors.black
+            ],
+            begin: Alignment.topLeft,
+            end: Alignment.bottomRight,
+          ),
+        ),
+      ),
+    );
   }
 }
