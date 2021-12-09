7514a6e09df1613ba042bc9bd9620c1809c02b63
Anand Singh <iamtheanandsingh@gmail.com>
2021-12-09 22:27:16 +0530

    Added firebase functionality and addded few music the bacendof h eapplication

--- muzik.app/lib/services/music_operations.dart
+++ muzik.app/lib/services/music_operations.dart
@@ -10,7 +10,7 @@ class MusicOperations{
         Music('Ateez',
               'https://i.pinimg.com/originals/1b/4c/40/1b4c403c71d27b7b5e3e94d987358a15.jpg',
               'Treasure',
-        'https://music.youtube.com/watch?v=05oOopLedWY&list=RDAMVM05oOopLedWY'),
+        'https://music.youtube.com/watch?v=05oOopLedWY&feature=share'),
         Music('Seventeen',
               'https://static.wikia.nocookie.net/girlgroup/images/8/8a/An_Ode_digital_cover.jpg/revision/latest?cb=20190916091132',
               'An Ode',
